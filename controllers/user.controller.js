c628992a766ac53612465bc1fdeb72751390f9e8
IIITIANRAJ <155481192+IIITIANRAJ@users.noreply.github.com>
2025-07-09 11:59:57 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -88,6 +88,8 @@ export const login = async (req , res) => {
             profile:user.profile
         }
 
+        console.log(token) ;
+
         return res.status(200).cookie("token", token, {maxAge: 1 * 24 * 60 * 60* 1000, httpsOnly: true, sameSite: 'strict'}).json({
             message:`Welcome back ${user.fullname}`,
             user,
