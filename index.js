691b359e9bd0c09a043df8bd677a77b38c7a47f7
IIITIANRAJ <155481192+IIITIANRAJ@users.noreply.github.com>
2025-07-09 11:31:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://job-web-app-frontend.onrender.com',
     credentials:true,
 }
 app.use(cors(corsOptions));
